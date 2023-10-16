0b895b6076577fe3f59b6dc15aab36c800a1ee4a
Elise Wei <elise.wei@gmail.com>
2023-10-16 22:35:31 +1300

    Handlebars ending state

--- week2/mon-hw/server/server.js
+++ week2/mon-hw/server/server.js
@@ -2,6 +2,9 @@
 import express from 'express'
 import * as Path from 'node:path'
 
+import { engine } from 'express-handlebars'
+import data from './data/data.js'
+
 // CREATE YOUR SERVER
 const server = express()
 
@@ -10,9 +13,25 @@ const publicFolder = Path.resolve('public')
 server.use(express.static(publicFolder))
 server.use(express.urlencoded({ extended: false}))
 
+// Use the hbs engine on our server
+server.engine('hbs', engine({ extname: 'hbs' }))
+
+// Connect the view engine to server
+server.set('view engine', 'hbs')
+server.set('views', Path.resolve('server/views'))
+
 // WRITE THOSE ROUTES
 server.get('/', (req, res) => {
-    res.sendFile(publicFolder + '/old.html')
+    // res.sendFile(publicFolder + '/old.html')
+
+    const viewData = {
+        artists: data.artists
+    }
+    res.render('home', viewData)
+})
+server.get('/about', (req, res) => {
+
+    res.render('aboutme')
 })
 
 export default server
