39e4d29f41fa77542ddc1e6816720c39b8ee97a6
Elise Wei <elise.wei@gmail.com>
2023-10-23 11:39:09 +1300

    w3d1a post-lecture

--- week3/mon-am/index.js
+++ week3/mon-am/index.js
@@ -1,1 +1,21 @@
 #!/usr/bin/env node
+
+import * as db from './db.js'
+
+console.log('args:', process.argv.slice(2))
+
+console.log(await db.getAllJars())
+
+console.log(await db.getJarById(2))
+
+console.log(await db.addJar({contents: 'plum butter'}))
+
+console.log('records deleted', await db.deleteJarById(2))
+
+console.log(await db.updateJar({
+  id: 1,
+  contents: 'blueberry jam',
+  canned_date: '2022-12-02'
+},))
+
+db.connection.destroy()
