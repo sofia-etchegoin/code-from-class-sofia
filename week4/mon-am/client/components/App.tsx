614a757a5ff3addeca5529c3fc03af24f142c838
Elise Wei <elise.wei@gmail.com>
2023-10-30 10:54:51 +1300

    post lecture

--- week4/mon-am/client/components/App.tsx
+++ week4/mon-am/client/components/App.tsx
@@ -1,11 +1,31 @@
+import Dog from './Dog'
+
 function App() {
+  const rosesDog = {
+    name: 'Alfie',
+    breed: 'humane society dog',
+    superpower: 'singing'
+  }
+  const mattsDog = {
+    name: 'Bean',
+    breed: 'whippet huntaway',
+    superpower: 'running away / being nervous'
+  }
+  const dogArr = [rosesDog, mattsDog]
   return (
     <div className="container">
-      <img
+      {/* <img
+        id="somepaw"
         className="spinner"
         alt="a spinning paw-print"
         src="/images/paw.png"
-      />
+      /> */}
+      <Dog {...mattsDog} />
+      <Dog {...rosesDog} />
+      
+      {dogArr.map(dogObj => {
+        return <Dog {...dogObj} />
+      })}
     </div>
   )
 }
