398ba6bd581b484ea25403db8427a24d2ae858f7
Gabriela Perez <gaby@perez.co.nz>
2023-11-02 12:04:15 +1300

    finished single input

--- week4/thurs-am/client/components/SingleInputForm.tsx
+++ week4/thurs-am/client/components/SingleInputForm.tsx
@@ -1,16 +1,40 @@
-//1. props for func
+import { useState } from 'react'
+interface Props {
+  handleAddScare: (newScares: string) => void
+}
 
-function SingleInputForm() {
+function SingleInputForm({ handleAddScare }: Props) {
+  const [text, setText] = useState('')
   //2. text state needs to change
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.value)
+    setText(event.target.value)
+  }
   //3. function to handle change in form
   //4. function to handle submit in form, prevent default and updates the text state, look at conditionals?
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    handleAddScare(text)
+    event.preventDefault()
+    setText('')
+    // do something with our 'text' state
+  }
+
   //5. set new text
   return (
     <>
-      <form>
+      <p>{text}</p>
+      <form onSubmit={handleSubmit}>
         <h2>Single Input Form</h2>
         <label htmlFor="new-scare">Scare Name </label>
-        <input type="text" name="new-scare" id="new-scare" />
+        <input
+          onChange={handleChange}
+          value={text}
+          type="text"
+          name="new-scare"
+          id="new-scare"
+        />
         <button type="submit">Submit</button>
       </form>
     </>
